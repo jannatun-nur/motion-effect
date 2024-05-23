fb42c23aaf15e3f2c78a8b0106ff2cc7b8dfce85
jannatun-nur <jannatunur03@gmail.com>
2024-05-23 20:35:30 +0600

    everything is done

--- src/App.jsx
+++ src/App.jsx
@@ -7,84 +7,12 @@ import Footer from "./Footer/Footer"
 import Navbar from "./Navbar/Navbar"
 import Users from "./Users/Users"
 import Visit from "./Visit/Visit"
-// import  { useLayoutEffect, useRef } from 'react';
-// import gsap from 'gsap-trial';
-// import { useGSAP } from "@gsap/react"
 
-// gsap.registerPlugin(useGSAP, ScrollSmoother);
-
-// function App() {
-
-//   const main = useRef();
-//   const smoother = useRef();
-
-
-
-//   useGSAP(
-//     () => {
-//       // create the smooth scroller FIRST!
-//       smoother.current = ScrollSmoother.create({
-//         smooth: 2, // seconds it takes to "catch up" to native scroll position
-//         effects: true, // look for data-speed and data-lag attributes on elements and animate accordingly
-//       });
-//       ScrollTrigger.create({
-//         trigger: '.box-c',
-//         pin: true,
-//         start: 'center center',
-//         end: '+=300',
-//         markers: true,
-//       });
-//     },
-//     { scope: main }
-//   );
- 
-
-//   return (
-//     <>
-//       <Navbar/>
-//       <Banner/>
-//       <Advertise/>
-//       <Users/>
-//       <Visit/>
-//       <Apply/>
-//       <Browser/>
-//       <Feature/>
-//       <Footer/>
-//     </>
-//   )
-// }
-
-// export default App
-
-
-// 
-
-import  { useLayoutEffect, useRef } from 'react';
-import gsap from 'gsap-trial';
-import { ScrollTrigger } from 'gsap-trial/ScrollTrigger';
-import { ScrollSmoother } from 'gsap-trial/ScrollSmoother';
-
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 function App() {
-  const smoother = useRef();
-
-  useLayoutEffect(() => {
-    smoother.current = ScrollSmoother.create({
-      wrapper: '#smooth-wrapper',
-      content: '#smooth-content',
-      smooth: 2, // seconds it takes to "catch up" to native scroll position
-      effects: true, // look for data-speed and data-lag attributes on elements and animate accordingly
-    });
-
-    return () => {
-      smoother.current.kill();
-    };
-  }, []);
 
   return (
-    <div id="smooth-wrapper">
-      <div id="smooth-content">
+    <>
         <Navbar />
         <Banner />
         <Advertise />
@@ -94,8 +22,8 @@ function App() {
         <Browser />
         <Feature />
         <Footer />
-      </div>
-    </div>
+     
+    </>
   );
 }
 
